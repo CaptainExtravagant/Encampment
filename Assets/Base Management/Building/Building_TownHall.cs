4240508a031939ec5665eb53afd7b32b2857ae6a
Olly <ripponolly@gmail.com>
2018-02-11 13:04:08 +0000

    Town Hall Panel + Functionality

--- Assets/Base Management/Building/Building_TownHall.cs
+++ Assets/Base Management/Building/Building_TownHall.cs
@@ -6,6 +6,47 @@ public class Building_TownHall : BaseBuilding, I_Building {
 	
 	void Awake ()
 	{
+		SetBuildingType (BUILDING_TYPE.BUILDING_TOWNHALL);
 		loadPath = "Buildings/BuildingTownHall";
+		workTime = 600.0f;
+		maxWorkingVillagers = 4;
+		activeTimer = workTime;
+	}
+
+	override public void WorkBuilding()
+	{
+		float resourceValue = 0;
+
+		for (int i = 0; i < workingVillagers.Count; i++) {
+			resourceValue += workingVillagers[i].GetCharacterInfo().characterAttributes.charm;;
+		}
+
+		resourceValue = (resourceValue / workingVillagers.Count) / 10;
+
+		for (int i = 0; i < resourceValue; i++) {
+			baseManager.characterScroll.GetComponent<CharacterDisplay>().Init(baseManager.SpawnVillager());
+		}
+
+		activeTimer = workTime;
+	}
+
+	public override void SetUpInfoPanel ()
+	{
+		float tempValue = 0;
+
+		for (int i = 0; i < workingVillagers.Count; i++)
+		{
+			infoPanel.GetComponentsInChildren<UnityEngine.UI.Text>()[i * 2].text = workingVillagers[i].GetCharacterInfo().characterName;
+			infoPanel.GetComponentsInChildren<UnityEngine.UI.Text>()[(i * 2) + 1].text = workingVillagers[i].GetCharacterInfo().characterAttributes.charm.ToString();
+
+			tempValue += workingVillagers[i].GetCharacterInfo().characterAttributes.charm;
+		}
+
+		tempValue = (tempValue / workingVillagers.Count) / 10;
+
+		infoPanel.GetComponentsInChildren<UnityEngine.UI.Text>()[9].text = Mathf.Ceil(tempValue);
+
+		infoPanel.GetComponentInChildren<UnityEngine.UI.Slider>().value = activeTimer / workTime;
+
 	}
 }
